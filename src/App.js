e0dffad39ceb5a3b8ff48dbf79f7a4e87b3285a1
MohamedPegasus <mohamedheragy89@gmail.com>
2022-10-11 15:33:47 +0200

    add switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {component } from 'react';
 import './App.css';
-import {BrowserRouter , Route,Routes} from 'react-router-dom';
+import {BrowserRouter , Route,Routes, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Services from './pages/Services';
 import Connect from './pages/Connect';
@@ -15,16 +15,14 @@ import Blog from './pages/Blog';
   return (
     <div className='App'>
         <div className='gradient__bg'>
-          <BrowserRouter>
-            <Routes>
+          <Switch>
               <Route exact path='/'element={ <Index /> } />
               <Route  path='/services'element={ <Services />} />
               <Route  path='/concet'element={ <Connect/>} />
               <Route  path='/blog'element={ <Blog />} />
               <Route  path='/login'element={ <Login/>} />
               <Route  path='/register'element={ <Register />} />
-            </Routes>
-            </BrowserRouter>
+            </Switch>
         </div>
       </div>
   );
