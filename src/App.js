f536fa9e7fdbce45ba0bda196e35953f089fd8c9
MohamedPegasus <mohamedheragy89@gmail.com>
2022-10-11 09:12:26 +0200

    run pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Blog from './pages/Blog';
 import  Navbar  from './Header';
+import  Header  from './containers/header/Header';
+import  WhatGPT3  from './containers/whatGPT3/WhatGPT3';
+import  Footer  from './containers/footer/Footer';
 
 
 
@@ -17,6 +20,10 @@ import  Navbar  from './Header';
       <div className='App'>
       <div className='gradient__bg'>
       <Routes>
+        <Navbar />
+            <Header />
+            <WhatGPT3 />
+            <Footer />
         <Route exact path='/'element={ <Home /> } />
         <Route  path='/services'element={ <Services />} />
         <Route  path='/concet'element={ <Connect/>} />
